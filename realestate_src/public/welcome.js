ffc29ff0331e81e14495b1f8682ba7798b2999e5
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-06-26 20:32:36 +0900

    0626 search add

--- realestate_src/public/welcome.js
+++ realestate_src/public/welcome.js
@@ -2,60 +2,85 @@ var loadingPhotos = false;
 var lastPhotoId = document.querySelector('#lastPhotoItem').dataset.id;
 
 function loadMorePhotos() {
-  if (loadingPhotos) return;
-  loadingPhotos = true;
-  var url = '/photos/more/' + lastPhotoId;
+    if (loadingPhotos) return;
+    loadingPhotos = true;
+    var url = '/photos/more/' + lastPhotoId;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      var scrollContainer = document.getElementById('scroll-container');
-      var newPhotos = response.photos.slice(0, 3);
-      newPhotos.forEach(function (photo) {
-        var newPhotosHtml = generatePhotoHtml(photo, response.lastPhotoId);
-        scrollContainer.insertAdjacentHTML('beforeend', newPhotosHtml);
-      });
-      if (response.lastPhotoId >= 20) {
-        scrollContainer.removeEventListener('scroll', scrollHandler);
-        loadingPhotos = false;
-        return;
-      }
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var scrollContainer = document.getElementById('scroll-container');
+            var newPhotos = response.photos.slice(0, 3);
+            newPhotos.forEach(function (photo) {
+                var newPhotosHtml = generatePhotoHtml(photo, response.lastPhotoId);
+                scrollContainer.insertAdjacentHTML('beforeend', newPhotosHtml);
+            });
+            if (response.lastPhotoId >= 20) {
+                scrollContainer.removeEventListener('scroll', scrollHandler);
+                loadingPhotos = false;
+                return;
+            }
 
-      loadingPhotos = false;
-      lastPhotoId = response.lastPhotoId;
-    } else {
-      console.error('Error: ' + xhr.status);
-    }
-  };
-  xhr.send();
+            loadingPhotos = false;
+            lastPhotoId = response.lastPhotoId;
+        } else {
+            console.error('Error: ' + xhr.status);
+        }
+    };
+    xhr.send();
 }
 
 function generatePhotoHtml(photo, lastPhotoId) {
-  var html = '<div class="photo-item" style="background-image: url(\'' + photo.url + '\');">' +
-    '<span class="photo-info">' +
-    '<span class="info-text">' + photo.s_add + '</span><br>' +
-    '<span class="info-text">' + photo.p_deposit + '</span>';
+    var html = '<div class="photo-item" style="background-image: url(\'' + photo.url + '\');">' +
+        '<span class="photo-info">' +
+        '<span class="info-text">' + photo.s_add + '</span><br>' +
+        '<span class="info-text">' + photo.p_deposit + '</span>';
 
-  if (photo.s_type === '월세') {
-    html += '<span class="info-text"> / ' + photo.p_month + '</span>';
-  }
-  html += '<br><span class="info-text">' + photo.updated_at.substr(0, 10) + '</span>' +
-    '</span>' +
-    '</div>';
+    if (photo.s_type === '월세') {
+        html += '<span class="info-text"> / ' + photo.p_month + '</span>';
+    }
+    html += '<br><span class="info-text">' + photo.updated_at.substr(0, 10) + '</span>' +
+        '</span>' +
+        '</div>';
 
-  html += '<input type="hidden" id="lastPhotoItem" data-id="' + lastPhotoId + '">';
+    html += '<input type="hidden" id="lastPhotoItem" data-id="' + lastPhotoId + '">';
 
-  return html;
+    return html;
 }
 
 var scrollContainer = document.getElementById('scroll-container');
-
 var scrollHandler = function () {
-  if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth) {
-    loadMorePhotos();
-  }
+    if (scrollContainer.scrollLeft + scrollContainer.clientWidth >= scrollContainer.scrollWidth) {
+        loadMorePhotos();
+    }
 };
 
 scrollContainer.addEventListener('scroll', scrollHandler);
+
+function searchProperties() {
+    var searchQuery = document.getElementById('search').value;
+    lastPhotoId = 0;
+    var url = '/photos/more/' + lastPhotoId + '?search=' + searchQuery;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var scrollContainer = document.getElementById('scroll-container');
+            scrollContainer.innerHTML = '';
+            response.photos.forEach(function (photo) {
+                var newPhotosHtml = generatePhotoHtml(photo, response.lastPhotoId);
+                scrollContainer.insertAdjacentHTML('beforeend', newPhotosHtml);
+            });
+            if (response.lastPhotoId >= 20) {
+                scrollContainer.removeEventListener('scroll', scrollHandler);
+            }
+            lastPhotoId = response.lastPhotoId;
+        } else {
+            console.error('Error: ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
