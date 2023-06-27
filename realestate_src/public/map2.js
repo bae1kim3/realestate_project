32bebde12a8b21a3b051c504ef94c69d08f53d34
baechanghyeon12 <qo99033@naver.com>
2023-06-27 16:52:54 +0900

    0627 map 수정

--- realestate_src/public/map2.js
+++ realestate_src/public/map2.js
@@ -106,6 +106,7 @@ function addfetch(url, selectedOption) {
             // 부모 요소 생성
             var accordion = document.createElement("div");
             accordion.className = "accordion";
+            accordion.id = "accordionExample";
 
             // 아코디언 아이템 생성
             var accordionItem = document.createElement("div");
@@ -114,6 +115,7 @@ function addfetch(url, selectedOption) {
             // 아코디언 헤더 생성
             var accordionHeader = document.createElement("h2");
             accordionHeader.className = "accordion-header";
+            accordionHeader.id = "headingOne";
 
             // 아코디언 버튼 생성
             var accordionButton = document.createElement("button");
@@ -129,6 +131,7 @@ function addfetch(url, selectedOption) {
 
             // 아코디언 컨텐츠 생성
             var accordionCollapse = document.createElement("div");
+            accordionCollapse.id = "collapseOne";
             accordionCollapse.className = "accordion-collapse collapse";
             accordionCollapse.setAttribute("aria-labelledby", "headingOne");
             accordionCollapse.setAttribute(
