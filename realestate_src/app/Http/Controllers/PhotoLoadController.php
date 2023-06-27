79d68aca197011c5c967538069865db97afc2b70
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-06-27 12:19:16 +0900

    0627 controller update

--- realestate_src/app/Http/Controllers/PhotoLoadController.php
+++ realestate_src/app/Http/Controllers/PhotoLoadController.php
@@ -9,6 +9,7 @@ class PhotoLoadController extends Controller
 {
     public function index()
     {
+        // 처음 페이지 진입 했을 때 출력시킬 사진 갯수
         $lastPhotoId = 5;
         $photos = Photo::join('s_infos', 's_infos.s_no', 'photos.s_no')
             ->where('mvp_photo', '1')
@@ -29,6 +30,7 @@ class PhotoLoadController extends Controller
             ->where('mvp_photo', '1')
             ->orderBy('photos.updated_at', 'desc');
 
+        // 검색기능
         if (!empty($searchQuery)) {
             $query->where(function ($q) use ($searchQuery) {
                 $q->where('s_infos.s_stai', 'LIKE', "%{$searchQuery}%") // 도로명 주소 검색 조건 추가
@@ -40,6 +42,7 @@ class PhotoLoadController extends Controller
             ->take($updatePhoto)
             ->get();
 
+        // 여태까지 출력된 사진의 총 갯수
         $lastPhotoId = $lastPhotoId + $updatePhoto;
 
         return response()->json(['photos' => $photos, 'lastPhotoId' => $lastPhotoId]);
