58a38fbc775ff229a182d48606831ed4a95cd448
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-06-27 15:24:03 +0900

    0627 welcome update

--- realestate_src/resources/views/welcome.blade.php
+++ realestate_src/resources/views/welcome.blade.php
@@ -4,13 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>펫방</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('nav.css') }}">
     <link rel="stylesheet" href="{{asset('welcome.css')}}">
+    <!-- favicon -->
+    <link rel="icon" href="{{asset('house-solid.png')}}">
     <!-- Styles -->
     <style>
       /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */
