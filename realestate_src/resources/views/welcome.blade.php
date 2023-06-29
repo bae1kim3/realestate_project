540349ba510ebb80c442118dc9ab81e4d1d17331
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-06-29 16:02:33 +0900

    0629 welcome update

--- realestate_src/resources/views/welcome.blade.php
+++ realestate_src/resources/views/welcome.blade.php
@@ -5,6 +5,13 @@
     <link rel="stylesheet" href="{{ asset('welcome.css') }}">
 
     <body class="antialiased">
+     <div style="height: 40vh; text-align: center; background-image: url({{ asset('abcd.png') }}); background-size: cover;">
+     <div style="line-height: 40vh;">
+    <label for="search" style="font-size: 20px; "class="font-bold text-black dark:text-white">매물 검색</label>
+    <input type="text" style="height: 40px;" class="rounded-lg px-2 py-1 dark:bg-gray-800 dark:text-white" name="search" id="search" placeholder="역이름, 주소로 검색해 주세요">
+    <button onclick="searchProperties()" style="font-size: 15px; width: 80px; height: 40px;" class="py-2 px-4 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 dark:bg-gray-400">검색</button>
+    </div>
+</div>
         <div
             class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (session('seller_license'))
@@ -28,13 +35,7 @@
             @endif
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                <div style="text-align: center;">
-                    <label for="search" class="text-black dark:text-white">매물 검색</label>
-                    <input type="text" class="rounded-lg px-2 py-1 dark:bg-gray-800 dark:text-white" name="search"
-                        id="search" placeholder="역이름, 주소로 검색해 주세요">
-                    <button onclick="searchProperties()"
-                        class="py-2 px-4 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 dark:bg-gray-400">검색</button>
-                </div>
+                
                 <br>
                 <br>
                 <br>
