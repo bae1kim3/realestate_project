6930dfe45ead96f361c0d6a578e140c231c5eabd
juyeong04 <yeongisgood@gmail.com>
2023-06-29 18:04:33 +0900

    0629

--- realestate_src/resources/views/welcome.blade.php
+++ realestate_src/resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                                 class="font-semibold text-gray-600 hover:text-gray-900 dark:text-white dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">매물올리기</a>
             @endif
                         </div>
-                    @else
+                    @elseif(session('u_id'))
                         <a href="{{ route('login') }}"
                             class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">로그인</a>
                         @if (Auth::user())
