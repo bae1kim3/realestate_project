7c5c8d8d227368165f2115d472c6d24f7d673bea
juyeong04 <yeongisgood@gmail.com>
2023-07-02 15:59:49 +0900

    0702 주영 비번변경 계정삭제js 수정

--- realestate_src/public/tab_menu.js
+++ realestate_src/public/tab_menu.js
@@ -3,9 +3,21 @@ const tabcon = document.querySelectorAll("[data-tab-content");
 tabs.forEach((tab)=> {
     tab.addEventListener("click", ()=>{
         const target = document.querySelector(tab.dataset.tabTarget);
+
+        if (tab.dataset.tabTarget === "#tab2") {
+            document.getElementById("pw_ch").style.display = "none";
+            document.getElementById("user_del").style.display = "none";
+        }else {
+            // 다른 탭을 클릭했을 때는 "pw_ch"와 "user_del" 요소를 보이게 처리
+            document.getElementById("pw_ch").style.display = "block";
+            document.getElementById("user_del").style.display = "block";
+        }
+        
         tabcon.forEach((tabc_all)=> {
             tabc_all.classList.remove("active");
         });
+
         target.classList.add("active");
     });
 });
+
--- realestate_src/resources/views/profile/update-profile-information-form.blade.php
+++ realestate_src/resources/views/profile/update-profile-information-form.blade.php
@@ -3,40 +3,40 @@
 <link rel="stylesheet" href="{{asset('mypagelist.css')}}">
 
 <div class="wrap">
-    @if(Illuminate\Support\Facades\Auth::user()->seller_license)
-            <h2 class="dark:text-white font-bold text-2xl pt-6">
-            {{ __('공인중개사 회원 정보') }}
-            </h2>
-    @else
-            <h2 class="dark:text-white font-bold text-2xl pt-6" style="padding-left:20px">
-            {{ __('개인 회원정보') }}
-            </h2>
-    @endif
+    <div class="in_wrap">
+        @if(Illuminate\Support\Facades\Auth::user()->seller_license)
+                <h2 class="dark:text-white font-bold text-2xl pt-6">
+                {{ __('공인중개사 회원 정보') }}
+                </h2>
+        @else
+                <h2 class="dark:text-white font-bold text-2xl pt-6" style="padding-left:20px">
+                {{ __('개인 회원정보') }}
+                </h2>
+        @endif
 
 
-    <div class='main'>
-        <div class='tabs text-bold dark:text-white'>
-            <div class='tab' data-tab-target='#tab1'>
-            @if(Auth::user()->seller_license)
-                <p>공인중개사 마이페이지</p>
-            @else
-                <p>개인 마이페이지</p>
-            @endif
-            </div>
-            <div class='tab' data-tab-target='#tab2'>
-            @if(Auth::user()->seller_license)
-                <p>내가 올린 매물</p>
-            @else
-                <p>찜목록</p>
-            @endif
+        <div class='main'>
+            <div class='tabs text-bold dark:text-white'>
+                <div class='tab' data-tab-target='#tab1'>
+                @if(Auth::user()->seller_license)
+                    <p>공인중개사 마이페이지</p>
+                @else
+                    <p>개인 마이페이지</p>
+                @endif
+                </div>
+                <div class='tab' data-tab-target='#tab2'>
+                @if(Auth::user()->seller_license)
+                    <p>내가 올린 매물</p>
+                @else
+                    <p>찜목록</p>
+                @endif
+                </div>
             </div>
         </div>
-    </div>
 
 
-    <div class='content' style="position:relative">
-        <div id='tab1' data-tab-content class='items active mx-8 dark:bg-gray-700'>
-
+        <div class='content' style="position:relative">
+            <div id='tab1' data-tab-content class='items active mx-8 dark:bg-gray-700'>
                 <form action="{{ route('update.userinfo.post') }}" id="frm" method="post" >
                 @csrf
                 {{-- <!-- Profile Photo -->
@@ -86,7 +86,7 @@
 
                 <!-- Name -->
                 <div class="col-span-6 sm:col-span-4">
-                    <x-label for="name" value="{{ __('이름') }}" class="mt-3" style="font-weight:700"/>
+                    <x-label for="name" value="{{ __('이름') }}" style="font-weight:700"/>
                     <x-input id="name" name="name" maxlength="20" type="text" class="mt-1 block w-full dark:bg-gray-600 dark:text-white" value="{{Auth::user()->name}}" placeholder="한글 이름으로 작성" />
 
                 </div>
@@ -112,7 +112,13 @@
                     <x-input id="s_lat" name="s_lat" type="hidden" class=" block w-full dark:bg-gray-600 dark:text-white"  />
                     <x-input id="s_log" name="s_log" type="hidden" class=" block w-full dark:bg-gray-600 dark:text-white"   />
                 </div>
-
+                @if(Illuminate\Support\Facades\Auth::user()->seller_license)
+                {{-- business name --}}
+                    <div class="col-span-6 sm:col-span-4">
+                        <x-label for="b_name" value="{{ __('상호명') }}" class="mt-3" style="font-weight:700"/>
+                        <x-input id="b_name" type="text" name="b_name" maxlength="20" class="mt-1 block w-full dark:bg-gray-600 dark:text-white" value="{{Auth::user()->b_name}}" placeholder="상호명 작성"/>
+                    </div>
+                @endif
 
                 @if(!(Illuminate\Support\Facades\Auth::user()->seller_license))
                     {{-- animal size --}}
@@ -130,8 +136,8 @@
                 <x-button wire:loading.attr="disabled" id="submit_btn" class="s_btn">
                     {{ __('저장') }}
                 </x-button>
+                </form>
 
-            </form>
                 <div>
                     <hr class="mt-8">
                     @foreach($errors->all() as $error)
@@ -158,110 +164,59 @@
                             <x-label for="seller_license" value="{{ __('공인중개사 라이센스') }}" class="mt-4" style="font-weight:700"/>
                             <x-input id="seller_license" name="seller_license" maxlength="10" type="text" class="mt-1 block w-full dark:bg-gray-600 dark:text-white" value="{{Auth::user()->seller_license}}" readonly  />
                         </div>
-                    {{-- business name --}}
-                        <div class="col-span-6 sm:col-span-4">
-                        <x-label for="b_name" value="{{ __('상호명') }}" class="mt-3" style="font-weight:700"/>
-                        <x-input id="b_name" type="text" name="b_name" maxlength="20" class="mt-1 block w-full dark:bg-gray-600 dark:text-white" value="{{Auth::user()->b_name}}" placeholder="상호명 작성"/>
                     @endif
-        </div>
-    </div>
-    {{-- 내가 올린 매물 --}}
-    @if(!(Illuminate\Support\Facades\Auth::user()->seller_license))
-        <div id='tab2' data-tab-content class='items'>
-            <div class="list">
-                @foreach($user as $val)
-                    <a href="{{ route('struct.detail', ['s_no' => $val->s_no]) }}">
-                    <div class="photo-item" style="background-image: url('{{ asset($val->url) }}');">
-                        <span class="photo-info">
-                            <span class="info-text">{{ $val->s_name }}</span><br>
-                            <span class="info-text">{{ $val->s_add }}</span>
-                        </span>
-                    </div>
-                    </a>
-                @endforeach
+                </div>
             </div>
+            {{-- 내가 올린 매물 --}}
+            @if((Illuminate\Support\Facades\Auth::user()->seller_license))
+                <div id='tab2' data-tab-content class='items'>
+                    <div class="list">
+                        @foreach($user as $val)
+                            <a href="{{ route('struct.detail', ['s_no' => $val->s_no]) }}">
+                            <div class="photo-item" style="background-image: url('{{ asset($val->url) }}');">
+                                <span class="photo-info">
+                                    <span class="info-text">{{ $val->s_name }}</span><br>
+                                    <span class="info-text">{{ $val->s_add }}</span>
+                                </span>
+                            </div>
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
         </div>
-    @endif
 
-    {{-- 이하 비밀번호 변경, 탈퇴 --}}
-    @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-        <div class="mt-10 sm:mt-0 dark:text-white" style="margin-top:20px; margin-left:40px;">
-            <h1>비밀번호를 변경하고 싶으신가요?</h1>
-            <br>
-            <a href="{{ route('profile.chk_phone_no') }}">
-            <x-button class="dark:bg-gray-600">비밀번호 변경</x-button>
-            </a>
+            {{-- 이하 비밀번호 변경, 탈퇴 --}}
+        <div class="under_con">
+            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                <div class="mt-10 sm:mt-0 dark:text-white" id="pw_ch">
+                    <h1>비밀번호를 변경하고 싶으신가요?</h1>
+                    <br>
+                    <a href="{{ route('profile.chk_phone_no') }}">
+                    <x-button class="dark:bg-gray-600">비밀번호 변경</x-button>
+                    </a>
+                </div>
+
+            @endif
+            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                <div class="mt-10 sm:mt-0 dark:text-white" id="user_del">
+                {{--@livewire('profile.delete-user-form')--}}
+                    <h1>계정을 삭제하시겠습니까?</h1>
+                    <br>
+                        <a href="{{ route('profile.chk_del_user') }}">
+                    <x-danger-button>회원 탈퇴
+                    </x-danger-button>
+                    </a>
+                </div>
+            @endif
         </div>
 
-    @endif
-    <x-section-border />
-    @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-        <div class="mt-10 sm:mt-0 dark:text-white" style="margin-left:40px;">
-        {{--@livewire('profile.delete-user-form')--}}
-            <h1>계정을 삭제하시겠습니까?</h1>
-            <br>
-                <a href="{{ route('profile.chk_del_user') }}">
-            <x-danger-button>회원 탈퇴
-            </x-danger-button>
-            </a>
-        </div>
-    @endif
 
+            <script src="{{asset('tab_menu.js')}}"></script>
+            <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+            <script src="{{asset('addr.js')}}"></script>
+            {{-- <script src="{{asset('mybuilding.js')}}"></script> --}}
 
-    <script src="{{asset('tab_menu.js')}}"></script>
-    <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
-    <script src="{{asset('addr.js')}}"></script>
-    {{-- <script src="{{asset('mybuilding.js')}}"></script> --}}
-
-
-    {{-- <script>
-    var isLoading = false;
-    var loadedItems = 0; // 이미 로드된 아이템 수
-
-    document.addEventListener('scroll', function() {
-        console.log('Scroll event');
-        if (!isLoading && isScrolledToBottom()) {
-            loadNextPage();
-        }
-    });
-
-    function isScrolledToBottom() {
-        return (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
-    }
-
-    function loadNextPage() {
-        var page = {{ $user_info->currentPage() }};
-        if (isLoading) return;
-        isLoading = true;
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/user/profile?page=' + (page + 1));
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                var photoItems = document.querySelector('#tab2 > div');
-
-                // 새로운 아이템을 4개씩 추가
-                var fragment = document.createDocumentFragment();
-                for (var i = 0; i < 4; i++) {
-                    if (loadedItems < response.length) {
-                        var newElement = document.createElement('div');
-                        newElement.innerHTML = response[loadedItems];
-                        fragment.appendChild(newElement);
-                        loadedItems++;
-                    }
-                }
-                photoItems.appendChild(fragment);
-
-                page++; // 페이지 번호 증가
-                isLoading = false;
-            } else {
-                console.log('Request failed. Status: ' + xhr.status);
-            }
-        };
-        xhr.send();
-    }
-    </script> --}}
+    </div>
 </div>
 </x-app-layout>
