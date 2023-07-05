028987ac58217d4438d187f1bfa58ca71f385729
juyeong04 <yeongisgood@gmail.com>
2023-07-05 09:41:06 +0900

    0705 주영 수정

--- realestate_src/resources/views/profile/delete-user-form.blade.php
+++ realestate_src/resources/views/profile/delete-user-form.blade.php
@@ -1,4 +1,4 @@
-<x-action-section>
+{{-- <x-action-section>
     <x-slot name="title">
         {{ __('Delete Account') }}
     </x-slot>
@@ -48,4 +48,4 @@
             </x-slot>
         </x-dialog-modal>
     </x-slot>
-</x-action-section>
+</x-action-section> --}}
