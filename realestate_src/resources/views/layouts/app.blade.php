290a6ba17fdef673d46124a37e4751c4e3621e17
qsczeadwx1 <qsczeadwx1@gmail.com>
2023-07-14 17:04:07 +0900

    0714 layout update

--- realestate_src/resources/views/layouts/app.blade.php
+++ realestate_src/resources/views/layouts/app.blade.php
@@ -1,52 +1,57 @@
+<!-- /*
+* Template Name: Property
+* Template Author: Untree.co
+* Template URI: https://untree.co/
+* License: https://creativecommons.org/licenses/by/3.0/
+*/ -->
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+<html lang="ko">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="author" content="Untree.co" />
 
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- favicon -->
     <link rel="icon" href="{{asset('house-solid.png')}}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <title>{{ __('펫 방') }}</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="bootstrap, bootstrap5" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}" />
+    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}" />
+
+    <link rel="stylesheet" href="{{asset('tiny-slider.css')}}" />
+    <link rel="stylesheet" href="{{asset('aos.css')}}" />
+    <link rel="stylesheet" href="{{asset('style.css')}}" />
 
     <!-- Styles -->
     @livewireStyles
+
+    <title>
+        펫 방 &mdash; 집구하자
+    </title>
+
 </head>
+<body>
+    {{-- 네비게이션 --}}
+    @include('navigation-menu')
 
-<body class="font-sans antialiased" x-data="{ darkMode: false }" x-init="if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    localStorage.setItem('darkMode', JSON.stringify(true));
-}
-darkMode = JSON.parse(localStorage.getItem('darkMode'));
-$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" x-cloak>
-    <div x-bind:class="{ 'dark': darkMode === true }" class="min-h-screen bg-gray-100">
-        @include('navigation-menu')
+    {{-- 컨텐츠 --}}
+    <main>
+        {{ $slot }}
+    </main>
 
-        <header class="bg-white shadow dark:bg-gray-800">
-            <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 dark:text-gray-100 dark:text-white">
-                <img src="{{ asset('logo.jpg') }}" alt="" style="width: 50px; height:50px">
-            </div>
-        </header>
-
-        <main class="bg-gray-100 dark:bg-gray-800">
-            {{ $slot }}
-        </main>
-
-        @stack('modals')
-
-        @livewireScripts
-    </div>
+    @livewireScripts
+    <script src="{{asset('bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('tiny-slider.js')}}"></script>
+    <script src="{{asset('aos.js')}}"></script>
+    <script src="{{asset('navbar.js')}}"></script>
+    <script src="{{asset('counter.js')}}"></script>
+    <script src="{{asset('custom.js')}}"></script>
 </body>
-
 </html>
