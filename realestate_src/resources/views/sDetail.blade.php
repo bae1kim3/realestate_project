c5035d95fe41a7f7ee8be31676ad32d4c8a4575b
kimyeongbeom <faer9876@naver.com>
2023-07-19 21:40:59 +0900

    디테일 수정

--- realestate_src/resources/views/sDetail.blade.php
+++ realestate_src/resources/views/sDetail.blade.php
@@ -28,78 +28,12 @@
     <link rel="stylesheet" href="{{asset('detail.css')}}">
 
 
-    <div class="site-wrap">
-        <div class="site-mobile-menu">
-            <div class="site-mobile-menu-header">
-                <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-                </div>
-            </div>
-        <div class="site-mobile-menu-body"></div>
-        </div> <!-- .site-mobile-menu -->
-
-        <div class="site-navbar mt-4" style="width:100%;">
-            <div class="container py-2" style="background-color: #005555; padding: 20px 30px; border-radius: 7px; box-shadow: 0 15px 30px -15px rgba(0, 0, 0, 0.1);">
-            <div class="row align-items-center">
-                <div class="col-8 col-md-8 col-lg-4">
-                <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0"><strong>Homeland<span class="text-danger">.</span></strong></a></h1>
-                </div>
-                <div class="col-4 col-md-4 col-lg-8">
-                <nav class="site-navigation text-right text-md-right" role="navigation">
-
-                    <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
-
-                    <ul class="site-menu js-clone-nav d-none d-lg-block">
-                        <li class="active">
-                            <a href="{{route('welcome')}}">Home</a>
-                        </li>
-                        @if (isset(session()->all()['auth']))
-                        <li class="has-children">
-                            <a href="properties.html">프로필</a>
-                            <ul class="dropdown arrow-top">
-                            <li><a href="#">내정보</a></li>
-                            <li class="has-children">
-                                <a href="#">Sub Menu</a>
-                                <ul class="dropdown">
-                                <li><a href="#">Menu One</a></li>
-                                <li><a href="#">Menu Two</a></li>
-                                <li><a href="#">Menu Three</a></li>
-                                </ul>
-                            </li>
-                            </ul>
-                        </li>
-                        @endif
-                        <li><a href="services.html">Services</a></li>
-                        <li><a href="about.html">About</a></li>
-                        @if (!session('u_id'))
-                        <li><a href="{{ route('login') }}">로그인</a></li>
-                        <li><a href="{{ route('register') }}" class="active">회원가입</a></li>
-                        @else
-                        <li><form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <a href="{{route('logout')}}" style="color: white"
-                                    onclick="event.preventDefault();
-                                                        this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </a>
-                                </li>
-                            </form></div>
-                        @endif
-                    </ul>
-                </nav>
-                </div>
-
-
-            </div>
-            </div>
-        </div>
-        </div>
+    <x-app-layout>
 
     <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('{{$mvp_photo->url}}');" data-aos="fade" data-stellar-background-ratio="0.5">
         <div class="container">
             <div class="row align-items-center justify-content-center text-center">
                 <div class="col-md-10">
-                    <span class="d-inline-block text-white px-3 mb-3 property-offer-type rounded">Property Details of</span>
                     <h1 class="mb-2">{{ $s_info->s_add }}</h1>
                 </div>
             </div>
@@ -285,105 +219,7 @@
         </div>
     </div>
     </div>
-<div class="site-footer" style="background-color:#efefef;">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="widget">
-                            <h3>Contact</h3>
-                            <address>43 Raymouth Rd. Baltemoer, London 3910</address>
-                            <ul class="list-unstyled links">
-                                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                                <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                                <li>
-                                    <a href="mailto:info@mydomain.com">info@mydomain.com</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.widget -->
-                    </div>
-                    <!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <div class="widget">
-                            <h3>Sources</h3>
-                            <ul class="list-unstyled float-start links">
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Services</a></li>
-                                <li><a href="#">Vision</a></li>
-                                <li><a href="#">Mission</a></li>
-                                <li><a href="#">Terms</a></li>
-                                <li><a href="#">Privacy</a></li>
-                            </ul>
-                            <ul class="list-unstyled float-start links">
-                                <li><a href="#">Partners</a></li>
-                                <li><a href="#">Business</a></li>
-                                <li><a href="#">Careers</a></li>
-                                <li><a href="#">Blog</a></li>
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">Creative</a></li>
-                            </ul>
-                        </div>
-                        <!-- /.widget -->
-                    </div>
-                    <!-- /.col-lg-4 -->
-                    <div class="col-lg-4">
-                        <div class="widget">
-                            <h3>Links</h3>
-                            <ul class="list-unstyled links">
-                                <li><a href="#">Our Vision</a></li>
-                                <li><a href="#">About us</a></li>
-                                <li><a href="#">Contact us</a></li>
-                            </ul>
-
-                            <ul class="list-unstyled social">
-                                <li>
-                                    <a href="#"><span class="icon-instagram"></span></a>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="icon-twitter"></span></a>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="icon-facebook"></span></a>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="icon-linkedin"></span></a>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="icon-pinterest"></span></a>
-                                </li>
-                                <li>
-                                    <a href="#"><span class="icon-dribbble"></span></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.widget -->
-                    </div>
-                    <!-- /.col-lg-4 -->
-                </div>
-                <!-- /.row -->
-
-                <div class="row mt-5">
-                    <div class="col-12 text-center">
-
-                        <p>
-                            Copyright &copy;
-                            <script>
-                                document.write(new Date().getFullYear());
-
-                            </script>
-                            . All Rights Reserved. &mdash; Designed with love by
-                            Pet Bang</a>
-                            <!-- License information: https://untree.co/license/ -->
-                        </p>
-                        <div>
-                            Made by B1K3
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.container -->
-        </div>
-        <!-- /.site-footer -->
+</x-app-layout>
 
 {{-- del jy 0718 --}}
 {{-- <script>
