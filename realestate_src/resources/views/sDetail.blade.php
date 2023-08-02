925742c2801a519d4e79079deba044f320ff915e
juyeong04 <yeongisgood@gmail.com>
2023-08-02 09:51:42 +0900

    0802주영 상세 백그라운드 변경

--- realestate_src/resources/views/sDetail.blade.php
+++ realestate_src/resources/views/sDetail.blade.php
@@ -75,7 +75,7 @@
     <x-app-layout>
 
         
-    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('{{$mvp_photo->url}}');" data-aos="fade" data-stellar-background-ratio="1">
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url('{{asset('images/hero_bg_1.jpg')}}');" data-aos="fade" data-stellar-background-ratio="1">
         <div class="container">
             <div class="row align-items-center justify-content-center text-center">
                 <div class="col-md-10">
